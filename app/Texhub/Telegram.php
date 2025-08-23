8c5dc157ae1c1994a09c9d70c3e2cee7c5612daf
TexusCode <texus.tj@gmail.com>
2025-08-23 14:20:51 +0500

    updated

--- app/Texhub/Telegram.php
+++ app/Texhub/Telegram.php
@@ -12,7 +12,7 @@ class Telegram extends WebhookHandler
 {
     public function start(): void
     {
-        $this->chat->message('Hello! Bot work!')->send();
+        // $this->chat->message('Hello! Bot work!')->send();
         Telegraph::message('hello world')->send();
         Telegraph::message('hello world')
             ->keyboard(Keyboard::make()->buttons([
@@ -24,11 +24,6 @@ class Telegram extends WebhookHandler
     public function deliver_chat_send($sms): void
     {
         $chat = TelegraphChat::find(2);
-        $chat->message($sms)->keyboard(Keyboard::make()->buttons([
-            Button::make('Delete')->action('delete')->param('id', '42'),
-            Button::make('open')->url('https://test.it'),
-            Button::make('Web App')->webApp('https://web-app.test.it'),
-            Button::make('Login Url')->loginUrl('https://loginUrl.test.it'),
-        ]))->send();
+        $chat->message('hello world')->send();
     }
 }
