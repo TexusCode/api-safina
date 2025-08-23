60d12a446e6e322df0eb729c0c3888824c1d3616
TexusCode <texus.tj@gmail.com>
2025-08-23 14:18:21 +0500

    updated

--- app/Texhub/Telegram.php
+++ app/Texhub/Telegram.php
@@ -13,12 +13,12 @@ class Telegram extends WebhookHandler
     public function start(): void
     {
         $this->chat->message('Hello! Bot work!')->send();
+        Telegraph::message('hello world')->send();
         Telegraph::message('hello world')
             ->keyboard(Keyboard::make()->buttons([
-                Button::make('open')->url('https://test.it'),
-                Button::make('Web App')->webApp('https://web-app.test.it'),
-                Button::make('Login Url')->loginUrl('https://loginUrl.test.it'),
-            ]))->send();
+                Button::make("👀 Open")->url('https://test.it'),
+                Button::make("📋 Copy to Clipboard")->copyText('https://example.com/share/123'),
+            ])->chunk(2))->send();
     }
 
     public function deliver_chat_send($sms): void
