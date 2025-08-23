c7c43fa258438ebe5a9b78b82bc7579777986e7e
TexusCode <texus.tj@gmail.com>
2025-08-23 14:11:28 +0500

    updated

--- app/Texhub/Telegram.php
+++ app/Texhub/Telegram.php
@@ -13,17 +13,6 @@ class Telegram extends WebhookHandler
     public function start(): void
     {
         $this->chat->message('Hello! Bot work!')->send();
-    }
-
-    public function deliver_chat_send($sms): void
-    {
-        $chat = TelegraphChat::where('chat_id', '-4815311836')->first();
-        $chat->message($sms)->keyboard(Keyboard::make()->buttons([
-            Button::make('Delete')->action('delete')->param('id', '42'),
-            Button::make('open')->url('https://test.it'),
-            Button::make('Web App')->webApp('https://web-app.test.it'),
-            Button::make('Login Url')->loginUrl('https://loginUrl.test.it'),
-        ]))->send();
         Telegraph::message('hello world')
             ->keyboard(Keyboard::make()->buttons([
                 Button::make('Delete')->action('delete')->param('id', '42'),
@@ -31,5 +20,16 @@ class Telegram extends WebhookHandler
                 Button::make('Web App')->webApp('https://web-app.test.it'),
                 Button::make('Login Url')->loginUrl('https://loginUrl.test.it'),
             ]))->send();
+    }
+
+    public function deliver_chat_send($sms): void
+    {
+        $chat = TelegraphChat::find(2);
+        $chat->message($sms)->keyboard(Keyboard::make()->buttons([
+            Button::make('Delete')->action('delete')->param('id', '42'),
+            Button::make('open')->url('https://test.it'),
+            Button::make('Web App')->webApp('https://web-app.test.it'),
+            Button::make('Login Url')->loginUrl('https://loginUrl.test.it'),
+        ]))->send();
     }
 }
