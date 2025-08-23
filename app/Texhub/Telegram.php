5fab17d0a71e7e2b7dedc8ae420c7920329d52ae
TexusCode <texus.tj@gmail.com>
2025-08-23 14:33:48 +0500

    updated

--- app/Texhub/Telegram.php
+++ app/Texhub/Telegram.php
@@ -11,20 +11,20 @@ class Telegram extends WebhookHandler
 {
     public function start(): void
     {
-        $this->chat->message('Hello! Bot work!')
-            ->keyboard(function (Keyboard $keyboard) {
-                return $keyboard
-                    ->button('Delete')->action('delete')->param('id', '42')
-                    ->button('open')->url('https://test.it')
-                    ->button('Web App')->webApp('https://web-app.test.it')
-                    ->button('Login Url')->loginUrl('https://loginUrl.test.it')
-                    ->button('Copy to Clipboard')->copyText('https://example.com/share/123');
-            })->send();
+        $this->chat->message('Hello! Bot work!')->send();
     }
 
     public static function deliver_chat_send($sms): void
     {
         $chat = TelegraphChat::where('chat_id', '-1002955286366')->first();
         $chat->message($sms)->send();
+        $chat->message($sms);
+        $chat->message('hello world')
+            ->keyboard(Keyboard::make()->buttons([
+                Button::make('Delete')->action('delete')->param('id', '42'),
+                Button::make('open')->url('https://test.it'),
+                Button::make('Web App')->webApp('https://web-app.test.it'),
+                Button::make('Login Url')->loginUrl('https://loginUrl.test.it'),
+            ]))->send();
     }
 }
