324afedae528f57ede147f62e66e796a343af63b
TexusCode <texus.tj@gmail.com>
2025-08-23 14:27:12 +0500

    updated

--- app/Texhub/Telegram.php
+++ app/Texhub/Telegram.php
@@ -14,7 +14,7 @@ class Telegram extends WebhookHandler
         $this->chat->message('Hello! Bot work!')->send();
     }
 
-    public function deliver_chat_send($sms): void
+    public static function deliver_chat_send($sms): void
     {
         $chat = TelegraphChat::where('chat_id', '-4815311836')->first();
         $chat->message($sms)->send();
