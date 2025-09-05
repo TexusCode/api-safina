0a53884150f3f46075b4cd84dd3de669af125cfa
TexusCode <texus.tj@gmail.com>
2025-09-06 00:12:00 +0500

    updated

--- app/Texhub/Telegram.php
+++ app/Texhub/Telegram.php
@@ -49,7 +49,7 @@ class Telegram extends \DefStudio\Telegraph\Handlers\WebhookHandler
         $phone = $order->customer->phone;
         $address = $order->customer->adress;
         $note = $order->note ?? "Нет заметок!";
-        $chat = TelegraphChat::find(2);
+        $chat = TelegraphChat::find(5);
         $customer = $order->customer;
         if ($customer->latitude && $customer->longitude) {
             $chat->location($customer->latitude, $customer->longitude)->send();
@@ -77,7 +77,7 @@ class Telegram extends \DefStudio\Telegraph\Handlers\WebhookHandler
         $phone = $order->customer->phone;
         $address = $order->customer->adress;
         $note = $order->note ?? "Нет заметок!";
-        $chat = TelegraphChat::find(2);
+        $chat = TelegraphChat::find(6);
         $customer = $order->customer;
         if ($customer->latitude && $customer->longitude) {
             $chat->location($customer->latitude, $customer->longitude)->send();
