ca0799c268ef43e3122ae383429395a0076e63ae
TexusCode <texus.tj@gmail.com>
2025-09-11 00:19:57 +0500

    updated

--- resources/views/livewire/pages/order-edit.blade.php
+++ resources/views/livewire/pages/order-edit.blade.php
@@ -43,6 +43,15 @@
         <div class="grid space-y-2 mt-5">
             <label class="block text-sm font-medium mb-2 dark:text-white">Выберите тариф</label>
 
+            <label for="tariff_8" class="flex p-3 w-full bg-white border border-gray-200 rounded-lg text-sm
+                focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700
+                dark:text-neutral-400">
+                <input type="radio" id="tariff_8" name="tariff" wire:model="tariff" value="8" required class="shrink-0 mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500
+                    checked:border-blue-500 disabled:opacity-50 disabled:pointer-events-none
+                    dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500
+                    dark:checked:border-blue-500 dark:focus:ring-offset-gray-800">
+                <span class="text-sm text-gray-500 ms-3 dark:text-neutral-400">8 сомони</span>
+            </label>
             <label for="tariff_10" class="flex p-3 w-full bg-white border border-gray-200 rounded-lg text-sm
                 focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700
                 dark:text-neutral-400">
