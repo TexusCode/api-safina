1e3d9b0ac880d862e3d8e6ca0ffe83a61e82b337
TexusCode <texus.tj@gmail.com>
2025-10-24 00:43:37 +0500

    updated

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,8 +12,8 @@
     <link rel="stylesheet" href="{{ asset('preline.css') }}">
 </head>
 
-<body class="bg-neutral-950 dark h-full">
-    <div class="h-full pb-24 overflow-scroll">
+<body class="bg-neutral-950 dark">
+    <div class="h-full pb-24">
         <div class="bg-neutral-950 w-full h-full max-w-sm mx-auto p-3 space-y-4 relative overflow-hidden">
             <div class="w-full flex justify-between items-center px-2 py-1.5 bg-neutral-800 rounded-xl">
                 <flux:button square variant="primary" color="blue" href="{{ route('profile') }}">
