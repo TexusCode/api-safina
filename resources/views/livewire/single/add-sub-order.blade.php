eba5b71e08cb7430986038ed7502eaf855629894
Mahmudi Shodmehr <mahmudishodmehr@Mac-mini-Mahmudi.local>
2025-12-06 17:06:18 +0500

    updated

--- resources/views/livewire/single/add-sub-order.blade.php
+++ resources/views/livewire/single/add-sub-order.blade.php
@@ -1,5 +1,5 @@
 <div class="space-y-4 w-full h-screen overflow-y-scroll">
-    <form wire:submit="add_suborder" class="overflow-y-auto space-y-3 w-full">
+    <form wire:submit="add_suborder" class="overflow-y-auto space-y-3 w-full mt-5">
         <input type="number" required wire:model="order"
             class="py-2.5 sm:py-3 px-4 block w-full border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
             placeholder="Номер заказа">
