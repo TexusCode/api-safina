c2dd1866f17a81f5890feaca68f4757cfc5ef2b8
Mahmudi Shodmehr <mahmudishodmehr@MacBook-Air-Mahmudi.local>
2025-12-19 07:52:26 +0500

    updated

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
@@ -12,4 +13,14 @@ class Review extends Model
         'rating',
         'feedback',
     ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
