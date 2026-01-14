d9b313acd5085c67f7223a267ddaa7aa7e280fc2
Mahmudi Shodmehr <mahmudishodmehr@Mac-mini-Mahmudi.local>
2026-01-14 17:22:40 +0500

    updated

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\CallHistoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// routes/api.php
 Route::post('/call-history-log', [CallHistoryController::class, 'store']);
+Route::post('/call-category', [CallHistoryController::class, 'storeCategory']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
